584a8e998174f48f61bce77b60d1d43c724a2b4f
Paul Barthmaier <paul@barthmaier.rocks>
2019-04-20 15:59:07 -0400

    interim

--- lib/register-blocks.php
+++ lib/register-blocks.php
@@ -6,24 +6,27 @@ add_action( 'init', __NAMESPACE__ . '\register_blocks', 40 );
 /**
  * Enqueue block editor only JavaScript and CSS.
  */
-function register_blocks() {	
+function register_blocks() {
 
-    // Fail if block editor is not supported
+	// Fail if block editor is not supported
 	if ( ! function_exists( 'register_block_type' ) ) {
 		return;
 	}
 
-    // List all of the blocks for your plugin
-    $blocks = ["jsforwpadvblocks/gallery"];
+	// List all of the blocks for your plugin
+	$blocks = [ 'jsforwpadvblocks/gallery' ];
 
-    // Register each block with same CSS and JS
-    foreach( $blocks as $block ) {
-        register_block_type( $block, [
-            'editor_script' => 'jsforwp-adv-gb-editor-js',
-            'editor_style'  => 'jsforwp-adv-gb-editor-css',
-            'style' => 'jsforwp-adv-gb-css'
-         ] );	  
-    }
+	// Register each block with same CSS and JS
+	foreach ( $blocks as $block ) {
+		register_block_type(
+			$block,
+			[
+				'editor_script' => 'jsforwp-adv-gb-editor-js',
+				'editor_style'  => 'jsforwp-adv-gb-editor-css',
+				'style'         => 'jsforwp-adv-gb-css',
+			]
+		);
+	}
 
 }
 
--- lib/register-scripts.php
+++ lib/register-scripts.php
@@ -9,9 +9,9 @@ add_action( 'init', __NAMESPACE__ . '\register_block_assets' );
 function register_block_assets() {
 
 	// Make paths variables so we don't write em twice ;)
-	$editor_js_path = '/assets/js/blocks.editor.js';
+	$editor_js_path    = '/assets/js/blocks.editor.js';
 	$editor_style_path = '/assets/css/blocks.editor.css';
-	$style_path = '/assets/css/blocks.style.css';
+	$style_path        = '/assets/css/blocks.style.css';
 
 	$js_dependencies = [ 'wp-plugins', 'wp-element', 'wp-edit-post', 'wp-i18n', 'wp-api-request', 'wp-data', 'wp-hooks', 'wp-plugins', 'wp-components', 'wp-blocks', 'wp-editor', 'wp-compose' ];
 
@@ -22,7 +22,7 @@ function register_block_assets() {
 		$js_dependencies,
 		filemtime( _get_plugin_directory() . $editor_js_path ),
 		true
-	);	
+	);
 
 	// Register editor only styles
 	wp_register_style(
@@ -31,7 +31,7 @@ function register_block_assets() {
 		[],
 		filemtime( _get_plugin_directory() . $editor_style_path )
 	);
-	
+
 	// Register shared editor and frontend styles
 	wp_register_style(
 		'jsforwp-adv-gb-css',
@@ -40,31 +40,30 @@ function register_block_assets() {
 		filemtime( _get_plugin_directory() . $style_path )
 	);
 
-	
 }
 
-add_action( "enqueue_block_editor_assets", __NAMESPACE__ . '\block_filters', 100 );
+add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\block_filters', 100 );
 /**
  * Enqueue block frontend JavaScript
  */
-function block_filters(){
+function block_filters() {
 
-	$filters_js_path = "/assets/js/blocks.filters.js";
-	$plugins_js_path = "/assets/js/plugins.editor.js";
+	$filters_js_path    = '/assets/js/blocks.filters.js';
+	$plugins_js_path    = '/assets/js/plugins.editor.js';
 	$plugins_style_path = '/assets/css/plugins.editor.css';
 
 	// Enqueue our block filters
-	wp_enqueue_script( 
-		"jsforwp-adv-gb-filters-js",
+	wp_enqueue_script(
+		'jsforwp-adv-gb-filters-js',
 		_get_plugin_url() . $filters_js_path,
-		['wp-hooks', 'lodash'],
+		[ 'wp-hooks', 'lodash' ],
 		filemtime( _get_plugin_directory() . $filters_js_path ),
 		true
 	);
 
 	// Enqueue our plugin JavaScript
-	wp_enqueue_script( 
-		"jsforwp-adv-gb-plugins-js",
+	wp_enqueue_script(
+		'jsforwp-adv-gb-plugins-js',
 		_get_plugin_url() . $plugins_js_path,
 		$js_dependencies,
 		filemtime( _get_plugin_directory() . $plugins_js_path ),
@@ -72,26 +71,26 @@ function block_filters(){
 	);
 
 	// Enqueue our plugin JavaScript
-	wp_enqueue_style( 
-		"jsforwp-adv-gb-plugins-css",
+	wp_enqueue_style(
+		'jsforwp-adv-gb-plugins-css',
 		_get_plugin_url() . $plugins_style_path,
 		[],
-		filemtime( _get_plugin_directory() . $plugins_style_path )		
+		filemtime( _get_plugin_directory() . $plugins_style_path )
 	);
 }
 
-add_action( "wp_enqueue_scripts", __NAMESPACE__ . '\frontend_assets' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\frontend_assets' );
 /**
  * Enqueue block frontend JavaScript
  */
-function frontend_assets(){
+function frontend_assets() {
 
-	$frontend_js_path = "/assets/js/blocks.frontend.js";
+	$frontend_js_path = '/assets/js/blocks.frontend.js';
 
-	wp_enqueue_script( 
-		"jsforwp-adv-gb-frontend-js",
+	wp_enqueue_script(
+		'jsforwp-adv-gb-frontend-js',
 		_get_plugin_url() . $frontend_js_path,
-		['wp-element'],
+		[ 'wp-element' ],
 		filemtime( _get_plugin_directory() . $frontend_js_path ),
 		true
 	);
